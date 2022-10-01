56d052e56325a04118d3ac019d7c68f9d9917c30
Brynn Shepherd <brynn@google.com>
2022-10-01 15:31:26 -0400

    html resposne

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const port = process.env.PORT || 3001;
 
 app.use('/api', require('./api'));
 
+app.get('/', (req, res) => res.type('html').send(html));
+
 app.listen(port, () =>
   console.log(`Andrew & Brynn's wedding website backend listening on ${port}!`),
 );
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Andrew & Brynn</title>
+  </head>
+  <body>
+    <p>Wedding website server WHAT IS HAPPENING</p>
+  </body>
+</html>
+`;
