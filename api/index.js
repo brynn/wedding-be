3c0b6b1868c55044fbd863e71ea005d9e8c49226
Brynn Shepherd <brynn@google.com>
2022-10-01 14:39:18 -0400

    fixes

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
 const db = require('../db');
-
 const router = require('express').Router();
 module.exports = router;
 
 // GET /api/rsvp
 router.get('/rsvp', async (req, res, next) => {
   try {
+    console.log('REQUEST: ', req);
     db.query('SELECT * from rsvp', (err, res) => {
       console.log('result of db query: ', err, res);
     });
+    const fakeRSVP = {
+      name: 'Testy McTest',
+      response: 'yes',
+      plus_one: false,
+    };
+    res.send(fakeRSVP);
   } catch (err) {
     next(err);
   }
 });
 
+// Default 404
 router.use((req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
