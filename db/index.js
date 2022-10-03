da9f287afbb7de191727a934bbe800ff3a85d51e
Brynn Shepherd <brynn@google.com>
2022-10-03 16:50:17 -0400

    pg client

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
-const {Pool} = require('pg');
+const {Client} = require('pg');
 
 const dbURL =
   'postgres://brynn:PC1ytD9OHhFTqC9VBLVi2EmYSdj8ief8@dpg-ccj1u7irrk09pi2jdfig-a/wedding';
 
 const db =
   process.env.NODE_ENV === 'production'
-    ? new Pool({dbURL})
-    : new Pool({
+    ? new Client({dbURL})
+    : new Client({
         user: 'brynn',
         host: 'localhost',
         database: 'postgres',
