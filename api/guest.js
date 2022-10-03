1b39367606fe30b4bc73ea9430bc665f7f7b4008
Brynn Shepherd <brynn@google.com>
2022-10-03 18:01:15 -0400

    ????

--- api/guest.js
+++ api/guest.js
@@ -15,9 +15,9 @@ router.get('/', async (req, res, next) => {
     };
     await db.connect();
     const {rows} = await db.query(query);
+    await db.end();
     if (rows?.length) {
       res.send(rows[0]);
-      await db.end();
     }
 
     // db.query(query, (err, result) => {
