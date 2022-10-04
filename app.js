e80faf8073f101433fe66d0ebb67bc3186829bbb
Brynn Shepherd <brynn@google.com>
2022-10-04 16:58:19 -0400

    comment out new cors

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 const port = process.env.PORT || 3001;
 module.exports = app;
 
-app.use(function (req, res, next) {
-  // res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, API-Key',
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, API-Key',
+//   );
+//   next();
+// });
 
 // Enables parsing of POST request body
 app.use(express.json());
