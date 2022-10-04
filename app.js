7a859972a298b019ffba12b90314c34b725c10b0
Brynn Shepherd <brynn@google.com>
2022-10-04 16:37:55 -0400

    comment out cors line

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001;
 module.exports = app;
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, API-Key',
