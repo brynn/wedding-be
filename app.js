ce5e99ae49602c022ed0b18c43b1a00da1e224de
Brynn Shepherd <brynn@google.com>
2022-10-04 16:14:14 -0400

    try with different cors

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 module.exports = app;
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key')
+  next()
+});
 
 // Enables parsing of POST request body
 app.use(express.json());
@@ -16,8 +21,8 @@ app.get('/', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     console.log('API KEY: ', process.env.API_KEY);
-    const apiKey = req.get('api_key');
-    console.log('from request: ', apiKey);
+    const apiKey = req.get('API-key');
+    console.log('from header: ', apiKey);
     if (!apiKey || apiKey !== process.env.API_KEY) {
       res.status(401).json({error: 'unauthorised'});
     } else {
@@ -26,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.use('/api', cors(), require('./api'));
+app.use('/api', require('./api'));
 
 app.use((err, req, res, next) => {
   console.error(err);
