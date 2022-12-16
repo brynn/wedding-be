4330cbecd655e340533b754fd20d7975c3d10635
Brynn Shepherd <brynn@google.com>
2022-12-16 15:24:26 -0500

    fix

--- api/rsvp.js
+++ api/rsvp.js
@@ -44,30 +44,28 @@ router.post('/', async (req, res, next) => {
     await db.none(`UPDATE guest SET name = $1, email = $2 WHERE id = $3`, [name, email, guest_id]);
 
     let new_plus_one = null;
-    if (plus_one_rsvp.response) {
-      const {guest_id, name, email, response, meal_choice, rehearsal_dinner} = plus_one_rsvp;
-      if (!guest_id) {
-        // We need to add a the new plus one to the guest and plus_ones tables first
-        new_plus_one = await db.one(
+    if (!plus_one_rsvp.guest_id && plus_one_rsvp.response) {
+      const {name, email, response, meal_choice, rehearsal_dinner} = plus_one_rsvp;
+      // We need to add a the new plus one to the guest and plus_ones tables first
+      new_plus_one = await db.one(
+        `
+        INSERT INTO guest (name, email, plus_one_allowed)
+        VALUES ($1, $2, $3)
+        RETURNING *
+        `,
+        [name, email, false],
+      );
+      if (new_plus_one) {
+        await db.none(
           `
-          INSERT INTO guest (name, email, plus_one_allowed)
-          VALUES ($1, $2, $3)
-          RETURNING *
-          `,
-          [name, email, false],
+          INSERT INTO plus_ones (guest_id, plus_one_id)
+          VALUES ($1, $2)`,
+          [guest_rsvp.guest_id, new_plus_one.id],
         );
-        if (new_plus_one) {
-          await db.none(
-            `
-            INSERT INTO plus_ones (guest_id, plus_one_id)
-            VALUES ($1, $2)`,
-            [guest_rsvp.guest_id, new_plus_one.id],
-          );
-        }
       }
 
-      // Now we can add the plus one's RSVP
-      const plus_one_id = new_plus_one?.id || plus_one_rsvp.guest_id;
+      // Now we can add the new plus one's RSVP
+      const plus_one_id = new_plus_one.id;
       new_plus_one_rsvp = await db.one(insertRSVPQuery, [
         plus_one_id,
         name,
@@ -76,13 +74,9 @@ router.post('/', async (req, res, next) => {
         meal_choice,
         rehearsal_dinner,
       ]);
-      await db.none(`UPDATE guest SET name = $1, email = $2 WHERE id = $3`, [
-        name,
-        email,
-        plus_one_id,
-      ]);
     } else if (!guest_rsvp.response && plus_one_rsvp?.guest_id) {
       // If the guest declines, make sure their plus one also declines
+      console.log('adding plus one rsvp decline');
       const {guest_id, name, email, meal_choice, rehearsal_dinner} = plus_one_rsvp;
       plus_one_rsvp = await db.one(insertRSVPQuery, [
         guest_id,
